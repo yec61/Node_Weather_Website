ca06f3a308595b38c23a00b083899880f64dfc13
Yunus Emre Cebeci <yec@Yunuss-MacBook-Air.local>
2024-03-08 18:59:19 +0000

    Remove unnnescessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+console.log('unstaged changes contol ')
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
