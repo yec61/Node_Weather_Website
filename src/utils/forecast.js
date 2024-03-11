27f77822350c91ada9a14424d11e016308ed228b
Yunus Emre Cebeci <yec@Yunuss-MacBook-Air.local>
2024-03-11 21:33:25 +0000

    Setup 2  app for Heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=' + latitude + ',' + longitude  
 
     request({ url, json: true }, (error, {body}) => {
         if (error) {
